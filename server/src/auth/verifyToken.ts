0cf31ba6b54da4e48956bb2707a8b258a0036e3d
Aigars Peda <aigarspeda@gmail.com>
2020-12-11 18:50:29 +0200

    Create verifyToken.ts

---
+++ server/src/auth/verifyToken.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import { User } from "../types";
+import { SECRET_KEY } from "../constance";
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // get auth header value
+  const bearerHeader = req.headers["authorization"];
+
+  if (
+    typeof bearerHeader !== "undefined" &&
+    bearerHeader.startsWith("Bearer ")
+  ) {
+    // split at the space
+    const bearerToken = bearerHeader.split(" ");
+    // get token from array
+    const token = bearerToken[1];
+    try {
+      const user = jwt.verify(token, SECRET_KEY) as User;
+      // @ts-ignore
+      req.user = user;
+
+      return next();
+    } catch (error) {
+      return res.status(401).json({ error: "not authorized" });
+    }
+  } else {
+    return res.status(401).json({ error: "not authorized" });
+  }
+};
