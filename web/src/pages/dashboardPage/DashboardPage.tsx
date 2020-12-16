a718eca6c27a36660ca108993056ce9f018df6a1
Aigars Peda <aigarspeda@gmail.com>
2020-12-16 06:30:13 +0200

    Update DashboardPage.tsx

--- web/src/pages/dashboardPage/DashboardPage.tsx
+++ web/src/pages/dashboardPage/DashboardPage.tsx
@@ -1,25 +1,56 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { dateFormatted } from "../../helpers/dateFormatted";
-import { getUsersTodos } from "../../redux/actions/todos";
+import { getUsersTodos, upDateTodoStatus } from "../../redux/actions/todos";
 import { RootState } from "../../redux/reducers";
 
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
 const DashboardPage: React.FC<Props> = (props) => {
-  const { todos, token, getUsersTodos } = props;
+  const { todos, token, getUsersTodos, upDateTodoStatus } = props;
 
   useEffect(() => {
     getUsersTodos(token);
   }, [getUsersTodos, token]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.checked);
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    todosId: number
+  ) => {
+    upDateTodoStatus(e.target.checked, token, todosId);
+  };
+
+  const eventTagsClass = (tag: string) => {
+    if (tag === "home") {
+      return "event-home";
+    } else if (tag === "fun") {
+      return "event-fun";
+    } else {
+      return "event-work";
+    }
+  };
+
+  const today = () => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    const d = new Date();
+    const day = days[d.getDay()];
+    const date = d.getDate();
+
+    return `${day} ${date}`;
   };
 
   return (
     <div className="dashboard-page">
-      <h1>Dashboard Page</h1>
+      <h1>Today's schedule</h1>
+      <h2>{today()}</h2>
       <ul className="event-card">
         {todos.length > 0 &&
           todos.map((todo) => {
@@ -28,11 +59,24 @@ const DashboardPage: React.FC<Props> = (props) => {
                 <input
                   type="checkbox"
                   checked={todo.completed}
-                  onChange={handleChange}
+                  onChange={(e) => handleChange(e, todo.id)}
                 />
-                <div className={`${todo.completed && "event-done"} event `}>
-                  <p className="event-text">{todo.description}</p>
-                  <p className="event-date">{dateFormatted(todo.created_on)}</p>
+                <div className={`${todo.completed && "event-done"} event`}>
+                  <div className="event-main-section">
+                    <p className="event-text">{todo.description}</p>
+                    <p className="event-date">
+                      {dateFormatted(todo.created_on)}
+                    </p>
+                  </div>
+                  <div className="event-tags">
+                    {todo.tags.map((tag, index) => {
+                      return (
+                        <span key={index} className={eventTagsClass(tag)}>
+                          {tag}
+                        </span>
+                      );
+                    })}
+                  </div>
                 </div>
               </li>
             );
@@ -47,6 +91,6 @@ const mapStateToProps = (state: RootState) => ({
   todos: state.todos.todos
 });
 
-const mapDispatchToProps = { getUsersTodos };
+const mapDispatchToProps = { getUsersTodos, upDateTodoStatus };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardPage);
