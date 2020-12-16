67aff4dba33702d85a3937ecdfc1c6be018887f9
Aigars Peda <aigarspeda@gmail.com>
2020-12-16 06:30:33 +0200

    Update todo.services.ts

--- web/src/services/todo.services.ts
+++ web/src/services/todo.services.ts
@@ -13,3 +13,27 @@ export const getTodos = async (token: string) => {
   const response: ITodo[] = await rawResponse.json();
   return response;
 };
+
+export const changeStatus = async (
+  status: boolean,
+  token: string,
+  todosId: number
+) => {
+  const rawResponse = await fetch(
+    `http://localhost:8000/todos/status/${todosId}`,
+    {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        completed: status
+      })
+    }
+  );
+
+  const response: ITodo[] = await rawResponse.json();
+  return response;
+};
