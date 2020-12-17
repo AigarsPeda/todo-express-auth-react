236e7aaa6dc1cce19b4f6396b98ce63d3d00cbda
Aigars Peda <aigarspeda@gmail.com>
2020-12-17 08:16:39 +0200

    Create AddIcon.tsx

---
+++ web/src/icons/AddIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const AddIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 1199 1199"
+      xmlns="http://www.w3.org/2000/svg"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={1.5}
+      {...props}
+    >
+      <path
+        d="M590.393 131.407v919.46M129.436 595.157h919.403"
+        fill="none"
+        stroke="#fff"
+        strokeWidth={133.3383758}
+      />
+    </svg>
+  );
+};
+
+export default AddIcon;
