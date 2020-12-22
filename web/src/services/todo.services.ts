5b58f0fa98384280bc8020ee1e7b3934b27bc591
Aigars Peda <aigarspeda@gmail.com>
2020-12-22 08:40:39 +0200

    Update todo.services.ts

--- web/src/services/todo.services.ts
+++ web/src/services/todo.services.ts
@@ -1,4 +1,4 @@
-import { ITodo } from "../types";
+import { IAPIMessage, ITodo } from "../types";
 
 export const getTodos = async (token: string) => {
   const rawResponse = await fetch("http://localhost:8000/todos", {
@@ -35,5 +35,41 @@ export const changeStatus = async (
   );
 
   const response: ITodo[] = await rawResponse.json();
+  return response;
+};
+
+export const newTodo = async (
+  description: string,
+  tags: string[],
+  token: string
+) => {
+  const rawResponse = await fetch(`http://localhost:8000/todos`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      description: description,
+      tags: tags
+    })
+  });
+
+  const response: ITodo = await rawResponse.json();
+  return response;
+};
+
+export const removeTodo = async (id: number, token: string) => {
+  const rawResponse = await fetch(`http://localhost:8000/todos/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  const response: IAPIMessage = await rawResponse.json();
   return response;
 };
