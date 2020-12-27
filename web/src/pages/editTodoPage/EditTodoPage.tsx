62985d14c561989f99c3bba9b52b8694ac9733d4
Aigars Peda <aigarspeda@gmail.com>
2020-12-27 20:51:48 +0200

    Update EditTodoPage.tsx

--- web/src/pages/editTodoPage/EditTodoPage.tsx
+++ web/src/pages/editTodoPage/EditTodoPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { RootState } from "../../redux/reducers";
 import { ITodo } from "../../types";
 
@@ -13,15 +13,35 @@ type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 const EditTodoPage: React.FC<Props> = (props) => {
   const { todos } = props;
   const { id } = useParams<IParamTypes>();
+  const history = useHistory();
   const [todo, setTodo] = useState<ITodo>();
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
-    setTodo(todos.find((t) => t.id === parseInt(id)));
+    const foundTodo = todos.find((t) => t.id === parseInt(id));
+    setTodo(foundTodo);
+    if (foundTodo) {
+      setDescription(foundTodo.description);
+    }
   }, [todos, id]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setDescription(value);
+  };
 
   return (
     <div className="edit-todo-page">
+      <button onClick={() => history.goBack()}>Back</button>
       {console.log(todo)}
+      {console.log(description)}
+      <input
+        type="text"
+        name="description"
+        value={description}
+        onChange={handleChange}
+        placeholder="Task Title..."
+      />
       <h1>{id}</h1>
       <h1>Yes</h1>
     </div>
