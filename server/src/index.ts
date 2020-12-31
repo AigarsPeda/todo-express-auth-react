1adf9859d1576ffdab986c893500e344d3a8c398
Aigars Peda <aigarspeda@gmail.com>
2020-12-31 05:10:35 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -166,13 +166,15 @@ app.put("/todo/:id", verifyToken, async (req: RequestWithUser, res) => {
 
       // try to find and update entry
       const foundTodo = await poll.query(
-        "UPDATE todos SET description = $1 WHERE id = $2 AND user_id = $3",
-        [description, id, user.user_id]
+        "UPDATE todos SET description = $1 WHERE id = $2 AND user_id = $3 RETURNING *",
+        [description, parseInt(id), user.user_id]
       );
+
+      // console.log(foundTodo.rows[0]);
 
       // check if something was updated
       if (foundTodo.rowCount) {
-        res.status(200).json({ message: "Todo was updated!" });
+        res.status(200).json(foundTodo.rows[0]);
       } else {
         res.status(401).json({ error: "forbidden" });
       }
