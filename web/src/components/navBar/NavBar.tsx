0605b128d4f97b91fd0b4bc9bdd9ab04fb019df7
Aigars Peda <aigarspeda@gmail.com>
2021-01-02 09:04:23 +0200

    Update NavBar.tsx

--- web/src/components/navBar/NavBar.tsx
+++ web/src/components/navBar/NavBar.tsx
@@ -2,19 +2,44 @@ import React from "react";
 import { connect } from "react-redux";
 import { RootState } from "../../redux/reducers";
 import { logOutUser } from "../../redux/actions/auth";
+import { matchPath, useHistory, useLocation } from "react-router-dom";
+
+import BackIcon from "../../icons/BackIcon";
 import ExitIcon from "../../icons/ExitIcon";
 
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
 const NavBar: React.FC<Props> = (props) => {
   const { isAuthenticated, logOutUser } = props;
+
+  const history = useHistory();
+  const location = useLocation();
+
+  const isMatch = matchPath(location.pathname, {
+    path: "/todo/:id",
+    exact: true,
+    strict: true
+  });
+
   return (
     <nav className="nav-bar">
       {isAuthenticated ? (
         <div className="nav-bar-controls">
-          <button onClick={logOutUser}>
-            <ExitIcon />
-          </button>
+          <div>
+            {isMatch && (
+              <button
+                onClick={() => history.goBack()}
+                className="nav-bar-back-button"
+              >
+                <BackIcon /> Back
+              </button>
+            )}
+          </div>
+          <div>
+            <button onClick={logOutUser} className="nav-bar-exit-button">
+              <ExitIcon />
+            </button>
+          </div>
         </div>
       ) : null}
     </nav>
