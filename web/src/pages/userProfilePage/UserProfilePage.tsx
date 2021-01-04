25ef230aa86302109f40d38bc02e800c9d39d98f
Aigars Peda <aigarspeda@gmail.com>
2021-01-04 08:43:58 +0200

    Create UserProfilePage.tsx

---
+++ web/src/pages/userProfilePage/UserProfilePage.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { connect } from "react-redux";
+import { RootState } from "../../redux/reducers";
+import { firstLetterUpper } from "../../helpers/firstLetterUpper";
+import { dateFormatted } from "../../helpers/dateFormatted";
+
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
+
+const UserProfilePage: React.FC<Props> = (props) => {
+  const { user } = props;
+  return (
+    <div className="user-profile-page">
+      <label className="user-name">
+        Username:
+        <p>{firstLetterUpper(user.username)}</p>
+      </label>
+      <label>
+        Created:
+        <p>{dateFormatted(user.created_on)}</p>
+      </label>
+      <label>
+        Email:
+        <p>{user.email}</p>
+      </label>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: RootState) => ({
+  user: state.user.user
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfilePage);
