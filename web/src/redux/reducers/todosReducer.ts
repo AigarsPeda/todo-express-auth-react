0acf366608025b60597b27ba65bea64178b495b5
Aigars Peda <aigarspeda@gmail.com>
2021-01-04 08:44:24 +0200

    Update todosReducer.ts

--- web/src/redux/reducers/todosReducer.ts
+++ web/src/redux/reducers/todosReducer.ts
@@ -5,7 +5,8 @@ import {
   DELETE_TODO,
   SetDataTypes,
   SET_TODOS_DATA,
-  UPDATE_TODO
+  UPDATE_TODO,
+  UPDATE_TODO_STATUS
 } from "../types";
 
 export interface ITodosInitialState {
@@ -29,6 +30,22 @@ export default (state = initialState, action: SetDataTypes) => {
       return {
         ...state,
         todos: [...state.todos, action.payload]
+      };
+    }
+
+    case UPDATE_TODO_STATUS: {
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === action.payload) {
+            return {
+              ...todo,
+              completed: !todo.completed
+            };
+          } else {
+            return todo;
+          }
+        })
       };
     }
 
