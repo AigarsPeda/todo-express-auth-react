696da2e8d9c298219a64225284d66b2f88714e92
Aigars Peda <aigarspeda@gmail.com>
2021-01-04 08:43:19 +0200

    Update NavBar.tsx

--- web/src/components/navBar/NavBar.tsx
+++ web/src/components/navBar/NavBar.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import { RootState } from "../../redux/reducers";
 import { logOutUser } from "../../redux/actions/auth";
-import { matchPath, useHistory, useLocation } from "react-router-dom";
+import { Link, matchPath, useHistory, useLocation } from "react-router-dom";
 
 import BackIcon from "../../icons/BackIcon";
 import ExitIcon from "../../icons/ExitIcon";
+import SettingsIcon from "../../icons/SettingsIcon";
 
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
@@ -16,7 +17,7 @@ const NavBar: React.FC<Props> = (props) => {
   const location = useLocation();
 
   const isMatch = matchPath(location.pathname, {
-    path: "/todo/:id",
+    path: "/",
     exact: true,
     strict: true
   });
@@ -26,7 +27,7 @@ const NavBar: React.FC<Props> = (props) => {
       {isAuthenticated ? (
         <div className="nav-bar-controls">
           <div>
-            {isMatch && (
+            {!isMatch && (
               <button
                 onClick={() => history.goBack()}
                 className="nav-bar-back-button"
@@ -35,8 +36,11 @@ const NavBar: React.FC<Props> = (props) => {
               </button>
             )}
           </div>
-          <div>
-            <button onClick={logOutUser} className="nav-bar-exit-button">
+          <div className="nav-bar-button">
+            <Link to={`/user`}>
+              <SettingsIcon />
+            </Link>
+            <button onClick={logOutUser}>
               <ExitIcon />
             </button>
           </div>
