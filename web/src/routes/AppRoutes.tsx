ae2187ee73c0aa743b00d88ea870a415c36afbb1
Aigars Peda <aigarspeda@gmail.com>
2021-01-05 09:10:48 +0200

    Update AppRoutes.tsx

--- web/src/routes/AppRoutes.tsx
+++ web/src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { RootState } from "../redux/reducers";
 import { connect } from "react-redux";
@@ -11,11 +11,12 @@ import DashboardPage from "../pages/dashboardPage/DashboardPage";
 import LoginPage from "../pages/loginPage/LoginPage";
 import SignupPage from "../pages/signupPage/SignupPage";
 import EditTodoPage from "../pages/editTodoPage/EditTodoPage";
+import UserProfilePage from "../pages/userProfilePage/UserProfilePage";
 
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
-const AppRoutes: React.FC<Props> = (props) => {
-  const { token } = props;
+const AppRoutes: React.FC<Props> = () => {
+  // const { token } = props;
 
   // useEffect(() => {
   //   // checking if token is valid and if it is setting in to axios header
@@ -26,10 +27,11 @@ const AppRoutes: React.FC<Props> = (props) => {
 
   return (
     <Switch>
-      <AuthRoute exact path="/" component={DashboardPage} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/signup" component={SignupPage} />
-      <Route exact path="/todo/:id" component={EditTodoPage} />
+      <AuthRoute exact path="/" component={DashboardPage} />
+      <AuthRoute exact path="/user" component={UserProfilePage} />
+      <AuthRoute exact path="/todo/:id" component={EditTodoPage} />
     </Switch>
   );
 };
