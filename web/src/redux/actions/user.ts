6c1cf90b711eec916b6a00b5bf7bbba090052043
Aigars Peda <aigarspeda@gmail.com>
2021-01-11 09:24:18 +0200

    Update user.ts

--- web/src/redux/actions/user.ts
+++ web/src/redux/actions/user.ts
@@ -1,14 +1,9 @@
-import { IUserLogIn, IUserSignUp } from "./../../types";
-import { SET_USER_PROFILE_IMAGE, SetUserTypes } from "./../types";
-import { AUTHENTICATE_USER, AuthenticateActionTypes } from "../types";
-
-import { ThunkAction } from "redux-thunk";
-import { RootState } from "../reducers";
 import { Action } from "redux";
+import { ThunkAction } from "redux-thunk";
 import { updateImage } from "../../services/user.services";
-// import { getUsersTodos } from "./todos";
-
-// ThunkAction<any, RootState, AuthenticateActionTypes | SetErrorActionTypes, Action<string>>
+import { RootState } from "../reducers";
+import { AuthenticateActionTypes } from "../types";
+import { SET_USER_PROFILE_IMAGE } from "./../types";
 
 type AppThunk<ReturnType = any> = ThunkAction<
   ReturnType,
@@ -17,22 +12,17 @@ type AppThunk<ReturnType = any> = ThunkAction<
   Action<string>
 >;
 
-// create new user
+// update users image
 export const updateUserProfilePicture = (
   file: File,
   token: string
-): AppThunk => async (_dispatch) => {
+): AppThunk => async (dispatch) => {
   try {
-    console.log("ei");
     const response = await updateImage(file, token);
-    console.log("RESPONSE: ", response);
-
-    // const { token, user } = response;
-    // authToken(token);
-    // dispatch({
-    //   type: AUTHENTICATE_USER,
-    //   payload: token
-    // });
+    dispatch({
+      type: SET_USER_PROFILE_IMAGE,
+      payload: response
+    });
   } catch (error) {
     console.log(error);
   }
